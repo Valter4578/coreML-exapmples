3e4aedfbdff40d2b39469d6c48454c50bd30ad35
Максим Алексеев <mrvalter566@gmail.com>
2018-12-05 17:50:08 +0700

    Add UI

---
+++ CoreMLDemo/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  CoreMLDemo
+//
+//  Created by Sai Kambampati on 14/6/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+}
