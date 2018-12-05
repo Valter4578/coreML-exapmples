f60cd195342531357281ae2fb0445e6386d0ac54
Максим Алексеев <mrvalter566@gmail.com>
2018-12-05 18:37:18 +0700

    Add functional of the work with camera and photo library

--- CoreMLDemo/ViewController.swift
+++ CoreMLDemo/ViewController.swift
@@ -9,5 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
+    // Outlets
+    @IBOutlet weak var classifer: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /// Runs when user click 'camera' button
+    @IBAction func camera(_ sender: Any) {
+        // Choose device camera
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        
+        present(cameraPicker,animated: true)
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
+    
+    
+
+}
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidяCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
