b36fbe6bf036dfde710233432b90586cf693fae6
Максим Алексеев <mrvalter566@gmail.com>
2018-12-07 19:25:03 +0700

    Create UI and conect Places205-GoogLeNet.

---
+++ SceneDetector/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  SceneDetector
+//
+//  Created by Максим Алексеев on 7/12/2018.
+//  Copyright © 2018 Pineapple team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
