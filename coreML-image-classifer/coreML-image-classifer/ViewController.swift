86936fb81224f17eb482682a3b959a6f6eb8a6d3
Максим Алексеев <mrvalter566@gmail.com>
2018-12-07 19:04:38 +0700

    Create project and conect model.

---
+++ coreML-image-classifer/coreML-image-classifer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreML-image-classifer
+//
+//  Created by Максим Алексеев on 07/12/2018.
+//  Copyright © 2018 Pineapple team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
