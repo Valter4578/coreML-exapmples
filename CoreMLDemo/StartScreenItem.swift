e078d66b39a92a5fea5effb69d3663ac7f71a450
Максим Алексеев <mrvalter566@gmail.com>
2018-12-24 19:40:28 +0700

    Add new starter screen. Also add new file StartScreenItem with data of tableView,StartScreenViewController.

---
+++ CoreMLDemo/StartScreenItem.swift
@@ -0,0 +1,13 @@
+//
+//  StartScreenItem.swift
+//  CoreMLDemo
+//
+//  Created by Максим Алексеев on 24/12/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class StartScreenItem {
+    var text = ""
+}
