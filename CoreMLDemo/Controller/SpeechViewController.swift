4d3927b2c01154da6d3ba2f1c066d129e03b4d23
Максим Алексеев <mrvalter566@gmail.com>
2020-03-03 21:43:02 +0700

    Create Speech to text screen

---
+++ CoreMLDemo/Controller/SpeechViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SpeechViewController.swift
+//  CoreMLDemo
+//
+//  Created by Максим Алексеев on 03.03.2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class SpeechViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
