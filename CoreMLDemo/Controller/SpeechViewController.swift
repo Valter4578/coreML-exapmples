3ee0b8c7373dd6a8a34ef50cbe45a3c128ce2924
Максим Алексеев <mrvalter566@gmail.com>
2020-03-03 22:08:40 +0700

    Finalyze function recordAndRecognizeSpeech() in speech view controller

--- CoreMLDemo/Controller/SpeechViewController.swift
+++ CoreMLDemo/Controller/SpeechViewController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+import Speech
 
 class SpeechViewController: UIViewController {
-
+    // MARK:- Outlets
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    // MARK:- Private properties
+    private let audioEngine = AVAudioEngine()
+    private let speechRecognizer = SFSpeechRecognizer()
+    private let request = SFSpeechAudioBufferRecognitionRequest()
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    // MARK:- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK:- Private methods
+    private func setupView() {
+        self.startButton.layer.cornerRadius = 10
     }
-    */
+    
+    
+    private func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
+            self.request.append(buffer)
+        }
+        
+        // Prepare and start recognizer
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            print(error)
+            return
+        }
+        
+        guard let recognizer = SFSpeechRecognizer() else { return }
+        if !recognizer.isAvailable { return }
+        
+        // Call recognizer task method
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { (result, error) in
+            if let result = result {
+                
+                let bestString = result.bestTranscription.formattedString
+                self.textLabel.text = bestString
+                
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
 
+    // MARK:- Actions
+    
+    @IBAction func startTapped(_ sender: UIButton) {
+        recordAndRecognizeSpeech()
+    }
+    
 }
