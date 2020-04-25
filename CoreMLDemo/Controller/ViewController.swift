1ab29325ca99e17609c2d252b25d47a641ccc575
Максим Алексеев <mrvalter566@gmail.com>
2020-04-25 23:37:30 +0700

    Code refactoring in GestureUIViewController

--- CoreMLDemo/Controller/ViewController.swift
+++ CoreMLDemo/Controller/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     //MARK: -  methods
     override func viewWillAppear(_ animated: Bool) {
         model = Inceptionv3()
-        print(imageView)
     }
     
     override func viewDidLoad() {
@@ -28,11 +27,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         imageView.layer.cornerRadius = 9
         imageView.clipsToBounds = true
     }
+    
     //MARK: - Private methods
     private func setupImageView() {
         self.imageView.layer.cornerRadius = 9
-        self.imageView.dropShadow()
-        
     }
     
     //MARK: Actions
@@ -42,7 +40,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     }
     
     /// Runs when user click 'camera' button
-    @IBAction func camera(_ sender: Any) {
+    @IBAction func openCamera(_ sender: Any) {
         // Choose device camera
         if !UIImagePickerController.isSourceTypeAvailable(.camera) {
             return
@@ -62,7 +60,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         picker.delegate = self
         picker.sourceType = .photoLibrary
         present(picker, animated: true)
-        self.imageView.dropShadow()
     }
     
     
@@ -77,31 +74,22 @@ extension ViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         // Get select image and put it's in constant "image"
-        /* Start here*/
         picker.dismiss(animated: true)
         classifer.text = "Analyzing Image..."
-        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
-            return
-        }
-        /*End here*/
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else { return }
         
         // Convert new image to 299x229
-        /* Start here */
         UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
         image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
         let newImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
-        /* End here */
         
         // Convert new image into a CVPixelBuffer
-        /* Start here */
         let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
         var pixelBuffer : CVPixelBuffer?
         let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard (status == kCVReturnSuccess) else {
-            return
-        }
-        /* End here */
+        guard (status == kCVReturnSuccess) else { return }
+
         
         CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
         let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
@@ -110,25 +98,17 @@ extension ViewController: UIImagePickerControllerDelegate {
         let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
         
         // Get all pixels in image and conver it's into iPhone RGB
-        /* Start here */
         context?.translateBy(x: 0, y: newImage.size.height)
         context?.scaleBy(x: 1.0, y: -1.0)
-        /* End here */
         
         // Render image
-        /* Start here */
         UIGraphicsPushContext(context!)
         newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
         UIGraphicsPopContext()
         CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
         imageView.image = newImage // Set for image space(Image View) changed the picture
-        /* End here */
         
-        
-
-        guard let prediction = try? model.prediction(image: pixelBuffer!) else {
-            return
-        }
+        guard let prediction = try? model.prediction(image: pixelBuffer!) else { return }
         
         classifer.text = "I think this is a \(prediction.classLabel)."
     }
